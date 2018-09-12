c9b0cdc0e94d093bee97539ceadc46402e080096
Alexis Jennings <ajennings@activecampaign.com>
2018-09-12 16:32:59 -0500

    HVAC Exercise pt 1

--- app/js/environment-controller.js
+++ app/js/environment-controller.js
@@ -1,14 +1,33 @@
-window.helloText = function() {
-  return 'Hello, World!';
-};
+class EnvironmentController {
+	constructor(hvac) {
+		this.hvac = hvac;
+		this.timer = 0;
+	}
+	
+	//called every minute
+	tick() {
+		this.timer += 1;
+		let temp = this.hvac.temp();
+		let tempInRange = temp > 65 && temp < 75;
+		let tempTooHigh = temp >= 75;
+		let tempTooLow = temp <= 65;
 
-window.hello = function() {
-  html = JST['app/templates/hello.us']({text: helloText()});
-  document.body.innerHTML += html;
-};
-
-if(window.addEventListener) {
-  window.addEventListener('DOMContentLoaded', hello, false);
-} else {
-  window.attachEvent('onload', hello);
+		if(tempInRange) {
+			this.hvac.heat(false);
+			this.hvac.cool(false);
+			this.hvac.fan(false);
+		} else if(tempTooLow) {
+			this.hvac.heat(true);
+			this.hvac.cool(false);
+			if(this.timer > 3) {
+				this.hvac.fan(true);
+			}
+		} else if(tempTooHigh) {
+			this.hvac.heat(false);
+			this.hvac.cool(true);
+			if(this.timer > 5) {
+				this.hvac.fan(true);
+			}
+		}
+	}
 }
--- spec/environment-controller-spec.js
+++ spec/environment-controller-spec.js
@@ -1,4 +1,135 @@
-describe(".helloText", function(){
-  When(function(){ this.result = helloText(); });
-  Then(function(){ expect(this.result).toEqual("Hello, World!"); });
+// Part 2
+// HVAC Wrapper --> use the existing interface or Web service version of HVAC
+
+describe("has temp", function(){
+	let HVAC;
+
+	beforeEach(function() {
+		HVAC = {
+			_temp: 70,
+			heatOn: false,
+			coolOn: false,
+			fanOn: false,
+			
+			heat(isOn) { 
+				this.heatOn = isOn;
+			},
+			cool(isOn) { 
+				this.coolOn = isOn; 
+			},
+			fan(isOn) {
+				this.fanOn = isOn;
+			},
+			temp() {
+				return this._temp;
+			},
+		}
+	});
+
+	it("head is off if temp is 70", () => {
+		HVAC._temp = 70;
+		
+		let environment = new EnvironmentController(HVAC);
+		environment.tick();
+		
+		expect(HVAC.heatOn).toBeFalsy();
+	});
+	
+	it("cool is off if temp is 66", () => {
+		HVAC._temp = 66;
+		
+		let environment = new EnvironmentController(HVAC);
+		environment.tick();
+		
+		expect(HVAC.coolOn).toBeFalsy();
+	});
+	
+	it("heat is on is temp is 65", () => {
+		HVAC._temp = 65;
+		
+		let environment = new EnvironmentController(HVAC);
+		
+		environment.tick();
+		
+		expect(HVAC.heatOn).toBeTruthy();
+	});
+	
+	it("cool is on if temp is 77", () => {
+		HVAC._temp = 77;
+		
+		let environment = new EnvironmentController(HVAC);
+		environment.tick();
+		
+		expect(HVAC.coolOn).toBeTruthy();
+		expect(HVAC.heatOn).toBeFalsy();
+	});
+	
+	it("fan is of if temp is between 65 and 75", () => {
+		HVAC._temp = 68;
+		
+		let environment = new EnvironmentController(HVAC);
+		environment.tick();
+		
+		expect(HVAC.heatOn).toBeFalsy();
+		expect(HVAC.coolOn).toBeFalsy();
+		expect(HVAC.fanOn).toBeFalsy();
+	});
+	
+	it("fan is off when the heat and cool is turned off", () => {
+		HVAC._temp = 74;
+	
+		let environment = new EnvironmentController(HVAC);
+		environment.tick();
+	
+		expect(HVAC.heatOn).toBeFalsy(); //heat is turned off
+		expect(HVAC.coolOn).toBeFalsy(); //cool is turned off
+		expect(HVAC.fanOn).toBeFalsy();
+	});
+	
+	it("fan turns on when heat is on AND 3 min after cool is turned off", () => {
+		HVAC._temp = 60;
+
+		let environment = new EnvironmentController(HVAC);
+
+		expect(environment.timer).toEqual(0);
+
+		environment.tick();
+		expect(HVAC.fanOn).toBeFalsy();
+
+		environment.tick();
+		expect(HVAC.fanOn).toBeFalsy();
+
+		environment.tick();
+		expect(HVAC.fanOn).toBeFalsy();
+
+		environment.tick();
+
+		expect(environment.timer).toEqual(4);
+		expect(HVAC.coolOn).toBeFalsy();
+		expect(HVAC.heatOn).toBeTruthy();
+		expect(HVAC.fanOn).toBeTruthy();
+	});
+	
+	it("fan truns on when cool is on AND 5 min after heat is turned off", () => {
+		HVAC._temp = 76;
+		
+		let environment = new EnvironmentController(HVAC);
+		
+		expect(environment.timer).toEqual(0);
+		
+		environment.tick();
+		expect(HVAC.fanOn).toBeFalsy();
+		
+		environment.tick();
+		environment.tick();
+		environment.tick();
+		environment.tick();
+		expect(HVAC.fanOn).toBeFalsy();
+		expect(environment.timer).toEqual(5);
+		
+		environment.tick();
+		expect(HVAC.heatOn).toBeFalsy();
+		expect(HVAC.coolOn).toBeTruthy();
+		expect(HVAC.fanOn).toBeTruthy();
+	});
 });
